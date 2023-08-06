b609b2867f8a95e57712e3c5d6ea271abb0cc652
Diana Salmenbayeva <salmebayevadiana@gmail.com>
2023-08-06 08:35:27 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='app-container'>
+      <h1>😸 Hello Diana! 😸</h1>
+      <h1>⏰⏰⏰⏰⏰</h1>
+    </div>
+  )
+}
+
+export default App
